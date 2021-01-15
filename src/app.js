bf967d031047427f78ecb11f92db800e47f51314
SANDIPKEDIA <47380324+SANDIPKEDIA@users.noreply.github.com>
2021-01-15 21:24:05 +0530

    Update app.js
    add Get method to Get all details of registered students with the help of asyncawait()

--- src/app.js
+++ src/app.js
@@ -10,18 +10,46 @@ app.use(express.json());
 
 
 //when client requesting server response with that parameteres(req,res)
-app.post("/students",(req,res) => { //create new student with post method(new student's registration)
-    console.log(req.body);
-    const user = new Student(req.body);
+// app.post("/students",(req,res) => { //create new student with post method(new student's registration)
+//     console.log(req.body);
+//     const user = new Student(req.body);
 
-    user.save().then(() => {
-        res.status(201).send(user);
-            }).catch((e) =>{
-                res.status(400).send(e);
-            })
+//     user.save().then(() =>{
+//         res.send(user);
+//             }).catch((e) =>{
+//                 res.send(e);
+//             })
     
-    // res.send("Hello from the Other Side........");
+//     // res.send("Hello from the Other Side........");
+// })
+
+//Another way to creation
+//Used async await
+app.post("/students",async(req,res) => {
+    try {
+        const user = new Student(req.body);
+        const createUser = await user.save();
+        res.status(201).send(createUser);
+    } catch (e) {
+        res.status(400).send(e);
+    }
 })
+
+
+
+//read the data by get method
+app.get("/students",async(req,res) => {
+    try {
+       const studentsData = await Student.find();
+       res.send(studentsData)
+        
+    } catch (e) {
+        res.send(e);
+    }
+})
+
+
+
 
 app.listen(port,() =>{
     console.log(`connection is setup at ${port}`);
